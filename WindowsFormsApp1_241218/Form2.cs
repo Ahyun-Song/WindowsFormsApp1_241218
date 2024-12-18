1b85ca8160d89d73a198c2e3ae7ad50157847e11
Ahyun-Song <yoon9113@naver.com>
2024-12-18 12:08:10 +0900

    coinTossResult

---
+++ WindowsFormsApp1_241218/Form2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1_241218
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+
+            bool userInput = true;
+            bool result = PlayGame(userInput);
+        }
+
+        public bool PlayGame(bool userInput)
+        {
+            Random random = new Random();
+            int coinTossResult = random.Next(2);
+            bool randomBool = (coinTossResult == 1);
+
+            if(userInput == randomBool)
+            {
+                textBox_print.Text = "승리";
+            }
+            else
+            {
+                textBox_print.Text = "패배";
+            }
+
+            return userInput == randomBool;
+        }
+    }
+}
--- WindowsFormsApp1_241218/Program.cs
+++ WindowsFormsApp1_241218/Program.cs
@@ -16,7 +16,7 @@ namespace WindowsFormsApp1_241218
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form2());
         }
     }
 }
